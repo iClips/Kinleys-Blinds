0a6fb9697d090f36f66aa987c0d64f71c7d07c2b
Iclips Tech <theronclintwilliam@gmail.com>
2025-02-13 01:32:14 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,29 @@ document.addEventListener('DOMContentLoaded', function () {
     initHeroAnimation();
     initScrollEffects();
     initIntoViewAnim();
+
+    observeElements('caption');    
 });
+
+const observeElements = (className) => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+      } else {
+        entry.target.classList.remove('visible');
+      }
+    });
+  }, {
+    threshold: 0.5,
+  });
+
+  const elements = document.querySelectorAll(`.${className}`);
+  elements.forEach((element) => {
+    observer.observe(element);
+  });
+};
+
 
 function toggleMenu() {
     const sideMenu = document.querySelector(".side-menu");
