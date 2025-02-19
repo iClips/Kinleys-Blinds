5ea69223f1c4b3b9d9475696e7391abc1bd6b95f
Iclips Tech <theronclintwilliam@gmail.com>
2025-02-19 23:57:29 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+        .then(reg => console.log("Service Worker Registered!", reg))
+        .catch(err => console.log("Service Worker Failed!", err));
+}
+
 let heroAnimationInterval = null;
 let isMenuOpen = false;
 let timeout = null;
