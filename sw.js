8ccf54d14b104ca6ba9d726ba8ed8e28eb57a7e1
Iclips Tech <theronclintwilliam@gmail.com>
2025-02-27 09:17:13 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,60 @@
 const CACHE_NAME = "kinleys-cache-v1";
 const OFFLINE_URL = "/offline.html";
 const assetsToCache = [
-    "/",
-    "/index.html",
-    "/assets/css/styles.css",
-    "/assets/js/jquery-1.12.1.min.js",
-    "/assets/js/bootstrap.min.js",
-    "/assets/js/jquery-1.12.1.min.js",
-    "/assets/js/script.js",
-    "/assets/js/jquery.fancybox.min.js",
-    "/assets/js/jquery.scrollUp.min.js",
-    "/assets/images/kinleys_logo.png",
-    "/offline.html"
+  "/",
+  "index.html",
+  "assets/css/styles.css",
+  "assets/js/jquery-1.12.1.min.js",
+  "assets/js/bootstrap.min.js",
+  "assets/js/jquery-1.12.1.min.js",
+  "assets/js/script.js",
+  "assets/js/jquery.fancybox.min.js",
+  "assets/js/jquery.scrollUp.min.js",
+  "assets/images/kinleys_logo.png",
+  "offline.html"
 ];
+
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(assetsToCache);
-        })
-    );
-    self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assetsToCache).then(() => {
+        return cache.keys().then(keys => {
+          return Promise.all(keys.map(key => {
+            return cache.get(key).then(response => {
+              if (response.headers.get('Content-Length') === '0') {
+                return fetch(key).then(networkResponse => {
+                  return cache.put(key, networkResponse.clone());
+                });
+              }
+            });
+          }));
+        });
+      });
+    })
+  );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(
-                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-            );
-        })
-    );
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
 });
 
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        fetch(event.request).catch(() => {
-            return caches.match(event.request) || caches.match(OFFLINE_URL);
-        })
-    );
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request).then(cacheResponse => {
+        if (cacheResponse) {
+          return cacheResponse;
+        } else {
+          return caches.match(OFFLINE_URL);
+        }
+      });
+    })
+  );
 });
